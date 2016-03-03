3210f094798c61247a13a3ec3b1d1ed762609a40
Sxia8 <sxia8@students.d125.org>
2016-03-03 15:04:54 -0600

    Added files via upload

---
+++ Desktop/lol.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class lol here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lol
+{
+    // instance variables - replace the example below with your own
+  public void main (String [] args)
+  {
+      System.out.print("Steven");
+    }
+}
